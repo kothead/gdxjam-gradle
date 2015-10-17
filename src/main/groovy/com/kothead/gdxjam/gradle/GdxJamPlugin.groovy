ac1a9b71273e2a6cd6bd1119e5aeec7e34e1c622
kothead <stopkillingshoggoths@gmail.com>
2015-10-17 23:58:27 +0500

    Create directory structure and empty plugin

---
+++ src/main/groovy/com/kothead/gdxjam/gradle/GdxJamPlugin.groovy
@@ -0,0 +1,10 @@
+package com.kothead.gdxjam.gradle
+
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+
+class GdxJamPlugin implements Plugin<Project> {
+    void apply(Project project) {
+        // apply your custom tasks here
+    }
+}
